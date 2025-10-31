dada80effabf12461730d0920047d1588049f243
Francisco Javier Mora Martin <fjmora.dev@outlook.es>
2025-10-31 12:54:48 +0100

    Fix: conexión a MongoDB con contraseña correcta

--- backend/server.js
+++ backend/server.js
@@ -12,7 +12,7 @@ app.use(express.json());    // Permite recibir datos JSON del frontend
 // Conectar con MongoDB local
 const mongoose = require("mongoose");
 require("dotenv").config();
-
+const mongoURI = process.env.MONGO_URI || "mongodb+srv://fjmoradev_db_user:Mariyalo9.@cluster0.ruqawbh.mongodb.net/sample_mflix?retryWrites=true&w=majority";
 
 mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
